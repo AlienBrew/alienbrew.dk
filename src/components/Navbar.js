5d29abc0f13b210759c3c8be20b5bfc2f98a741b
Michael Klingenberg <mkl@madhukar.dk>
2018-05-10 19:54:41 +0200

    logo 110

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="AlienBrew" style={{ width: '88px' }} />
+            <img src={logo} alt="AlienBrew" style={{ width: '110px' }} />
           </figure>
         </Link>
       </div>
