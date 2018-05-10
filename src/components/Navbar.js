cdb7e1b13031926a17d4f93f3c37c60306d78576
Michael Klingenberg <mkl@madhukar.dk>
2018-05-10 19:59:37 +0200

    css update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="AlienBrew" style={{ width: '110px' }} />
+            <img src={logo} alt="AlienBrew" style={{ width: '90px' }} />
           </figure>
         </Link>
       </div>
