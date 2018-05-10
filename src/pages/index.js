3fcdac784aa4dfe510cf58344d3678384ef35da7
Michael Klingenberg <mkl@madhukar.dk>
2018-05-10 15:57:09 +0200

    dd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import logo from '../img/alienbrew_logo.svg'
 
 export default class IndexPage extends React.Component {
   render() {
@@ -11,6 +12,7 @@ export default class IndexPage extends React.Component {
       <section className="section">
         <div className="container">
           <div className="content">
+            <img src={logo} />
             <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
           </div>
           {posts
