228683dba396a56dd9ee6ad8c314fbc13d776113
Michael Klingenberg <mkl@madhukar.dk>
2018-05-10 19:47:31 +0200

    css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.png'
+import logo from '../img/logo.svg'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -10,7 +10,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="AlienBrew" style={{ width: '20%' }} />
+            <img src={logo} alt="AlienBrew" style={{ width: '88px' }} />
           </figure>
         </Link>
       </div>
