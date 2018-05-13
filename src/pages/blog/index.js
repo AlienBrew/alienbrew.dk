e5f6e7c54b00fbb1883ba16c9d3167e68cfb68e0
Michael Klingenberg <mkl@madhukar.dk>
2018-05-13 11:07:31 +0200

    blog page update layout

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -11,14 +11,14 @@ export default class BlogPage extends React.Component {
       <section className="section">
         <div className="container">
           <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+            <h1 className="has-text-weight-bold is-size-2">Nyeste indlæg</h1>
           </div>
           {posts
             .filter(post => post.node.frontmatter.templateKey === 'blog-post')
             .map(({ node: post }) => (
               <div
                 className="content"
-                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+                style={{ border: '1px solid #000000', padding: '2em 4em' }}
                 key={post.id}
               >
                 <p>
@@ -33,7 +33,7 @@ export default class BlogPage extends React.Component {
                   <br />
                   <br />
                   <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
+                    Læs mere →
                   </Link>
                 </p>
               </div>
