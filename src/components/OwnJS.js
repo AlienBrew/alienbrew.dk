b2b58ad12433be3d5a80579be8d6413b5cd441ce
Michael Klingenberg <mkl@madhukar.dk>
2018-05-18 10:49:55 +0200

    own js file

---
+++ src/components/OwnJS.js
@@ -0,0 +1,12 @@
+function hamburgerHelper(){
+  document.querySelector('.navbar-burger').addEventListener("click", toggleNav);
+
+  function toggleNav() {
+          var nav = document.querySelector('.navbar-menu');
+          if(nav.className == "navbar-menu") {
+              nav.className = "navbar-menu is-active";
+          } else {
+              nav.className = "navbar-menu";
+          }
+  }
+}
