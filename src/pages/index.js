afcceff859266c47337608ba6a44c55e1787c498
Michael Klingenberg <mkl@madhukar.dk>
2018-05-18 12:59:56 +0200

    back agian

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,32 +11,7 @@ export default class IndexPage extends React.Component {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
 
-    return document.addEventListener("DOMContentLoaded", function() {
-
-// Get all "navbar-burger" elements
-      var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
-
-// Check if there are any navbar burgers
-      if ($navbarBurgers.length > 0) {
-
-        // Add a click event on each of them
-        $navbarBurgers.forEach(function($el) {
-          $el.addEventListener("click", function() {
-
-            // Get the target from the "data-target" attribute
-            var target = $el.dataset.target;
-            var $target = document.getElementById(target);
-
-            // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-            $el.classList.toggle("is-active");
-            $target.classList.toggle('is-active');
-
-          });
-        });
-      }
-
-    });
-
+    return (
       <section className="section">
         <div className="container">
           <div className="content">
