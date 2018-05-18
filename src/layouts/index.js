181341bbc15ace0387c7e4f9d9ddeea026d87dd9
Michael Klingenberg <mkl@madhukar.dk>
2018-05-18 10:30:38 +0200

    d

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,6 +11,7 @@ const TemplateWrapper = ({ children }) => (
     <Navbar />
     <div>{children()}</div>
   </div>
+  <script>
   function hamburgerHelper(){
     document.querySelector('.navbar-burger').addEventListener("click", toggleNav);
 
@@ -23,6 +24,7 @@ const TemplateWrapper = ({ children }) => (
             }
     }
 }
+</script>
 )
 
 TemplateWrapper.propTypes = {
